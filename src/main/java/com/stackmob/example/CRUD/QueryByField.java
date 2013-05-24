63460a81bb89fcc6dc9a16fb2522cf3fce50a392
stilltikin <github@xsiv.org>
2013-05-23 19:41:21 -0700

    q

--- src/main/java/com/stackmob/example/CRUD/QueryByField.java
+++ src/main/java/com/stackmob/example/CRUD/QueryByField.java
@@ -36,47 +36,59 @@ import java.util.*;
 
 public class QueryByField implements CustomCodeMethod {
 
-  @Override
-  public String getMethodName() {
-    return "CRUD_Query_By_Field";
-  }
+	@Override
+	public String getMethodName() {
+		return "CRUD_Query_By_Field";
+	}
 
-  @Override
-  public List<String> getParams() {
-    return Arrays.asList("sid");
-  }
+	@Override
+	public List<String> getParams() {
+		return Arrays.asList("sid","start","end");
+	}
 
-  @Override
-  public ResponseToProcess execute(ProcessedAPIRequest request, SDKServiceProvider serviceProvider) {
-    Map<String, List<SMObject>> feedback = new HashMap<String, List<SMObject>>();
-    Map<String, String> errMap = new HashMap<String, String>();
+	@Override
+	public ResponseToProcess execute(ProcessedAPIRequest request, SDKServiceProvider serviceProvider) {
+		Map<String, List<SMObject>> feedback = new HashMap<String, List<SMObject>>();
+		Map<String, String> errMap = new HashMap<String, String>();
+		
+		String sid = request.getParams().get("sid");
+		if (Util.hasNulls(sid)){
+			return Util.badRequestResponse(errMap);
+		}
+		String val = request.getParams().get("start");
+		if (!Util.hasNulls(val)){
+			try {
+				long start = Long.valueOf(val).longValue();
+			} catch(NumberFormatException nfe) {
+				return Util.internalErrorResponse("invalid start value", nfe, errMap);	// http 500 - internal server error
+			}
+		} else {
+			long start = 0;
+		}
+		val = request.getParams().get("end");
 
-    String sid = request.getParams().get("sid");
-    if (Util.hasNulls(sid)){
-      return Util.badRequestResponse(errMap);
-    }
-
-    List<SMCondition> query = new ArrayList<SMCondition>();
-    DataService ds = serviceProvider.getDataService();
-    List<SMObject> results;
-
-    try {
-      // Create a query condition to match all photo objects to the `sid` that was passed in
-      query.add(new SMEquals("story_id", new SMString(sid)));
-      query.add(new SMNotEqual("state", new SMString("D")));
-      results = ds.readObjects("photos", query);
-
-      if (results != null && results.size() > 0) {
-        feedback.put(sid, results);
-      }
-
-    } catch (InvalidSchemaException ise) {
-      return Util.internalErrorResponse("invalid_schema", ise, errMap);  // http 500 - internal server error
-    } catch (DatastoreException dse) {
-      return Util.internalErrorResponse("datastore_exception", dse, errMap);  // http 500 - internal server error
-    }
-
-    return new ResponseToProcess(HttpURLConnection.HTTP_OK, feedback);
-  }
+		//resultFilter
+		List<SMCondition> query = new ArrayList<SMCondition>();
+		DataService ds = serviceProvider.getDataService();
+		List<SMObject> results;
+		
+		try {
+			// Create a query condition to match all photo objects to the `sid` that was passed in
+			query.add(new SMEquals("story_id", new SMString(sid)));
+			query.add(new SMNotEqual("state", new SMString("D")));
+			results = ds.readObjects("photos", query);
+			
+			if (results != null && results.size() > 0) {
+				feedback.put(sid, results);
+			}
+			
+		} catch (InvalidSchemaException ise) {
+			return Util.internalErrorResponse("invalid_schema", ise, errMap);	// http 500 - internal server error
+		} catch (DatastoreException dse) {
+			return Util.internalErrorResponse("datastore_exception", dse, errMap);	// http 500 - internal server error
+		}
+		
+		return new ResponseToProcess(HttpURLConnection.HTTP_OK, feedback);
+	}
 
 }
