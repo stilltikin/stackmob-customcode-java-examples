d8e11294c75fa868dd229203c7b87db706126c28
stilltikin <github@xsiv.org>
2013-05-23 21:10:09 -0700

    c

--- src/main/java/com/stackmob/example/CRUD/QueryByField.java
+++ src/main/java/com/stackmob/example/CRUD/QueryByField.java
@@ -90,7 +90,7 @@ public class QueryByField implements CustomCodeMethod {
 		List<SMOrdering> qorder = Arrays.asList(new SMOrdering("taken", OrderingDirection.DESCENDING));
 		List<String> qfields = Arrays.asList("photos_id", "caption", "back", "width", "height", "photo", "taken");
 
-//		ResultFilters resultFilter = new ResultFilters(start, end, qorder, qfields);
+		ResultFilters resultFilter = new ResultFilters(start, end, qorder, qfields);
 	
 		List<SMCondition> query = new ArrayList<SMCondition>();
 		DataService ds = serviceProvider.getDataService();
